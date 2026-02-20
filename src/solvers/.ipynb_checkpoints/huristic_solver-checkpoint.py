c11a4583fd9dbb7f1a911d6a5c7fcea0b098ff15
Mirdev <mepbphh@gmail.com>
2026-02-20 20:38:20 +0900

    feat: 엔진-솔버 분리 및 규칙 파라미터화 통합 완료 (#6)

--- src/solvers/.ipynb_checkpoints/huristic_solver-checkpoint.py
+++ src/solvers/.ipynb_checkpoints/huristic_solver-checkpoint.py
@@ -1,7 +1,6 @@
 from collections import Counter
-from itertools import permutations, product
 
-class HeuristicSolver:
+class PositionalFrequencySolver:
     """
     [Naive Bayesian 구조의 변형]
     각 위치(Position)별 숫자의 독립 출현 확률을 계산하는 대신, 
@@ -13,19 +12,8 @@ class HeuristicSolver:
         self.candidates = self._generate_all_candidates()
 
     def _generate_all_candidates(self):
-        # 엔진의 allow_duplicates 설정에 따라 생성 방식 결정
-        if self.engine.allow_duplicates:
-            # 중복 허용: 0000 ~ 9999 (10^4개)
-            all_cands = list(product(range(10), repeat=self.engine.digits))
-        else:
-            # 중복 비허용: nPr (10P4 = 5040개)
-            all_cands = list(permutations(range(10), self.engine.digits))
-    
-        # 엔진의 allow_leading_zero 설정에 따라 필터링
-        if not self.engine.allow_leading_zero:
-            all_cands = [c for c in all_cands if c[0] != 0]
-            
-        return all_cands
+        from itertools import permutations
+        return list(permutations(range(10), self.digits))
 
     def update_candidates(self, guess, feedback):
         self.candidates = [
@@ -57,21 +45,3 @@ class HeuristicSolver:
                 best_guess = cand
 
         return best_guess
-        
-    def solve(self, secret):
-        turns = 0
-        while True:
-            turns += 1
-            # 엔트로피 솔버는 turns가 필요하므로 가변 인자로 처리하거나 내부에서 판단
-            if hasattr(self, 'get_best_guess') and self.__class__.__name__ == 'EntropySolver':
-                guess = self.get_best_guess(turns)
-            else:
-                guess = self.get_best_guess()
-                
-            feedback = self.engine.get_feedback(secret, guess)
-            
-            # 정답 판정 (엔진의 digits 설정 사용)
-            if feedback == (self.engine.digits, 0):
-                return turns
-                
-            self.update_candidates(guess, feedback)
